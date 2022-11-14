ee9249a042fc356d3acf72e7e7fd95585982c28c
Callum Waters <cmwaters19@gmail.com>
2022-11-14 09:53:30 +0100

    handle inbound votes

--- state.go
+++ state.go
@@ -1,23 +1,72 @@
 package consensus
 
 import (
-	"crypto/ed25519"
+	"fmt"
 	sync "sync"
+	"time"
+
+	"github.com/tendermint/tendermint/types"
+)
+
+const (
+	ProposeStep uint8 = iota
+	PrevoteStep
+	PrecommitStep
+	CommitStep
 )
 
 type state struct {
-	sync.RWMutex
-	height uint64
+	// immutable fields
+	namespace   string
+	height     uint64
+	params     *SynchronyParams
+	validators *types.ValidatorSet
+
+	mtx   sync.RWMutex
 	round uint32
-	step uint8	
-
-	pubkey ed25519.PublicKey
+	step  uint8
 }
 
-func newState() *state {
-	return &state{}
+func newState(height uint64, round uint32) *state {
+	return &state{
+		height: height,
+		round:  round,
+		step:   ProposeStep,
+	}
 }
 
-func (s *state) setPubkey(pubkey ed25519.PublicKey) {
-	s.pubkey = pubkey
+func (s *state) increment() {
+
+}
+
+func (s *state) scheduleTimeout() {
+	s.mtx.RLock()
+	defer s.mtx.RUnlock()
+	height, round, step := s.height, s.round, s.step
+	time.AfterFunc(calcTimeout(round, step, s.params), s.timeoutFunc(height, round, step))
+}
+
+func (s *state) timeoutFunc(height uint64, round uint32, step uint8) func() {
+	return func() {
+		s.mtx.RLock()
+		if s.height != height || s.round != round || s.step != step {
+			s.mtx.RUnlock()
+			return
+		}
+		s.mtx.RUnlock()
+		s.increment()
+	}
+}
+
+func calcTimeout(round uint32, step uint8, params *SynchronyParams) time.Duration {
+	switch step {
+	case ProposeStep:
+		return params.Propose.AsDuration() + (time.Duration(round) * params.ProposeDelta.AsDuration())
+	case PrevoteStep, PrecommitStep:
+		return params.Vote.AsDuration() + (time.Duration(round) * params.Vote.AsDuration())
+	case CommitStep:
+		return params.Commit.AsDuration()
+	default:
+		panic(fmt.Sprintf("unexpected step %d", step))
+	}
 }
--- util.go
+++ util.go
@@ -1,10 +1,33 @@
 package consensus
 
-import "crypto/ed25519"
+import (
+	"crypto/ed25519"
+	"fmt"
+
+	"google.golang.org/protobuf/proto"
+)
 
 // Default to ed25519
 func DefaultVerifyFunc() VerifyFunc {
 	return func(publicKey, message, signature []byte) bool {
 		return ed25519.Verify(publicKey, message, signature)
 	}
+}
+
+func CannonicalizedVoteBytes(v *Vote, payloadID []byte, namespace string) []byte {
+	if v == nil {
+		panic("nil vote")
+	}
+	msg := &SignRequest{
+		Type:      SignRequest_Type(v.Type),
+		Height:    v.Height,
+		Round:     v.Round,
+		PayloadId: payloadID,
+		Namespace: namespace,
+	}
+	bz, err := proto.Marshal(msg)
+	if err != nil {
+		panic(fmt.Errorf("marshal sign request bytes: %w", err))
+	}
+	return bz
 }
